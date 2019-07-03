dbcd7f5c49af94d5ab437277de2585e44e298e72
wangdan3024 <wangdan3024@qq.com>
2019-07-03 14:47:06 +0800

    前端环境搭建

---
+++ html-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+  );
+}
+
+export default App;
